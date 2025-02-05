6abffa7c55343fc90932d8142ca8e2ff2fb5da74
coletto0007 <luanncoletto@gmail.com>
2025-02-05 15:00:16 -0300

    script-js

---
+++ 05-02/script.js
@@ -0,0 +1,6 @@
+
+let nome = pronpt ("qual seu nome")
+let idade = prompt("digite sua idade")
+document.write("seja bem vindo: " + nome + ", " + "sua idade : " + idade + "anos" )
+document.write("<br><br>")
+document.write(`seja bem vindo: ${nome}, sua idade e: ${idade} anos`)
