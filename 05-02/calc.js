1903b292c798876b46d64f3ae65c3032c7415d10
coletto0007 <luanncoletto@gmail.com>
2025-02-05 15:45:39 -0300

    calculadora

---
+++ 05-02/calc.js
@@ -0,0 +1,12 @@
+// duas variaveis
+// num1 e num2
+//pergunta para usuario
+let num1 = number(pronpt("digite um numero"))
+let num2 = number(prompt("digite outro numero"))
+
+console.log(typeof num1)
+console.log(typeof num2)
+
+
+// impromir resposta
+console.log(`a soma e ${num1 + num2}`)
--- 05-02/script.js
+++ 05-02/script.js
@@ -1,4 +1,3 @@
-
 let nome = pronpt ("qual seu nome")
 let idade = prompt("digite sua idade")
 document.write("seja bem vindo: " + nome + ", " + "sua idade : " + idade + "anos" )
