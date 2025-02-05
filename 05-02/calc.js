f3d66233d5eb9d7cd0aee109bc291cc7d1c61400
coletto0007 <luanncoletto@gmail.com>
2025-02-05 16:10:00 -0300

    Casagit statusgit status

--- 05-02/calc.js
+++ 05-02/calc.js
@@ -1,12 +1,20 @@
 // duas variaveis
 // num1 e num2
 //pergunta para usuario
-let num1 = number(pronpt("digite um numero"))
-let num2 = number(prompt("digite outro numero"))
 
-console.log(typeof num1)
-console.log(typeof num2)
+let num1 = Number(prompt("digite um numero"));
+let num2 = Number(prompt("digite outro numero"));
+
+//operaçao
+let soma = num1 + num2;
 
 
 // impromir resposta
-console.log(`a soma e ${num1 + num2}`)
+console.log(`a soma e ${soma}`);
+
+let num3 = Number(prompt("digite um numero"));
+let num4 = Number(prompt("digite outro numero"));
+
+let sub = num3 - num4;
+
+console.log(`a subitecao e ${sub}`);
---
+++ 05-02/exer1.js
@@ -0,0 +1,8 @@
+/*
+- armasene a palavra 'tom' em uma let "tom";    
+- armasene a palavra 'hanks' em uma let "firstom"
+- armasene a palavra 'cruise' em uma let "secondtom"    
+- declare uma let "myfavoritetom" que resebe nome    
+     esoaco, e sobrenome do tom que voce mais gosta
+- exiba a "myfavoritetom" no console do browser
+*/
