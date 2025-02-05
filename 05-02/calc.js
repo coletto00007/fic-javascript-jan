93ed5591fc29d74842321c0a1d89fa91e1c8d30c
coletto0007 <luanncoletto@gmail.com>
2025-02-05 16:20:18 -0300

    calculadora

--- 05-02/calc.js
+++ 05-02/calc.js
@@ -18,3 +18,17 @@ let num4 = Number(prompt("digite outro numero"));
 let sub = num3 - num4;
 
 console.log(`a subitecao e ${sub}`);
+
+let num5 = Number(prompt("digite um numero"));
+let num6 = Number(prompt("digite outro numero"));
+
+let mut = num5 * num6;
+
+console.log(`a mutiolicacao e ${mut}`);
+
+let num7 = Number(prompt("digite um numero"));
+let num8 = Number(prompt("digite outro numero"));
+
+let div = num7 / num8;
+
+console.log(`a divisao e ${div}`);
